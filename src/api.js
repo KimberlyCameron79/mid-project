474fe5fd519154584109653cd7cffb0700ecfe13
KimberlyCameron79 <cameronkd7@live.com>
2022-10-27 16:04:50 -0400

    mid-proj

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': 'ff3c86e4cemsh5b9a0c3fcc37e8ap1e11f8jsn43859391059b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
