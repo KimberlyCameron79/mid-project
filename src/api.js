c0f96dbd0b4c3383b123a2c6fc6dabc1e283e16e
KimberlyCameron79 <cameronkd7@live.com>
2022-10-28 16:53:00 -0400

    mid-proj

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 export const geoApiOptions = {
-	method: 'GET',
+	method: "GET",
 	headers: {
-        'X-RapidAPI-Key': 'ff3c86e4cemsh5b9a0c3fcc37e8ap1e11f8jsn43859391059b',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        "X-RapidAPI-Key": "ff3c86e4cemsh5b9a0c3fcc37e8ap1e11f8jsn43859391059b",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
 	}
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ff5294d1179817dddf81a87dcb30cf37";
