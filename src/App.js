0fd672d9bcc6ade45e43f7b153cbf2471f5e2417
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-10-30 23:07:15 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
-import Search from './components/search/Search';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
-import CurrentWeather from './components/current-weather/CurrentWeather';
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import { useState } from 'react';
 
+export default function App() {
 
-function App() {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-  const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=ff5294d1179817dddf81a87dcb30cf37`
 
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
+  const searchLocation = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
       })
-      .catch(console.log);
-  };
-
-  return (
-    <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <forecast data={forecast} />}
-    </div>
-  );
+      setLocation('')
+  }
 }
+      return (
+        <div className="app">
+          <div className="search">
+            <input 
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            onKeyPress={searchLocation}
+            placeholder="Enter Location"
+            type="text" />
+          </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            <h1>65°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">65°F</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className="bold">20%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p className="bold">12 MPH</p>
+              <p>Wind Speed</p>
+            </div>
 
-export default App;
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+  }
