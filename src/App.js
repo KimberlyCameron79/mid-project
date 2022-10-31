b37a88d8910e17afd033be0533243fe144a820f9
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-10-31 00:34:19 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -16,49 +16,57 @@ export default function App() {
         console.log(response.data)
       })
       setLocation('')
+    }
   }
-}
-      return (
-        <div className="app">
-          <div className="search">
-            <input 
-            value={location}
-            onChange={e => setLocation(e.target.value)}
-            onKeyPress={searchLocation}
-            placeholder="Enter Location"
-            type="text" />
-          </div>
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text" />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>65°F</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
-          <div className="bottom">
-            <div className="feels">
-              <p className="bold">65°F</p>
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              <p className="bold">20%</p>
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              <p className="bold">12 MPH</p>
-              <p>Wind Speed</p>
+
+     {/* bottom display upon data input */}
+
+          {data.name != undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
             </div>
 
 
+          }
 
-          </div>
+
         </div>
       </div>
     </div>
 
   );
-  }
+
+}
---
+++ src/components/Data.js
@@ -0,0 +1,48 @@
+
+const data ={
+"coord": {
+    "lon": -79.9959,
+    "lat": 40.4406
+    },
+    "weather": [
+    {
+    "id": 804,
+    "main": "Clouds",
+    "description": "overcast clouds",
+    "icon": "04n"
+    }
+    ],
+    "base": "stations",
+    "main": {
+    "temp": 285.3,
+    "feels_like": 284.5,
+    "temp_min": 281.97,
+    "temp_max": 287.75,
+    "pressure": 1017,
+    "humidity": 74
+    },
+    "visibility": 10000,
+    "wind": {
+    "speed": 1.06,
+    "deg": 132,
+    "gust": 1.09
+    },
+    "clouds": {
+    "all": 100
+    },
+    "dt": 1667176739,
+    "sys": {
+    "type": 2,
+    "id": 2008038,
+    "country": "US",
+    "sunrise": 1667130445,
+    "sunset": 1667168381
+    },
+    "timezone": -14400,
+    "id": 5206379,
+    "name": "Pittsburgh",
+    "cod": 200
+    }
+;
+
+export default data; 
